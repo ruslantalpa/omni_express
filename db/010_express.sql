c7e3d8ae6effff6329a2ac7d2d17cde62b45263c
Ruslan Talpa <ruslan.talpa@subzero.cloud>
2023-06-06 15:55:51 +0300

    add regexp route capabilities

--- db/010_express.sql
+++ db/010_express.sql
@@ -2,10 +2,85 @@ drop schema if exists express cascade;
 create schema express;
 
 set search_path to express;
-create domain express.request as omni_httpd.http_request;
+-- create domain express.request as omni_httpd.http_request;
+
 create domain express.outcome as omni_httpd.http_outcome;
 -- create domain express.response as omni_httpd.http_response;
-create type route as (name text, method text, path text, handler text, priority integer);
+create type path as (value text, is_regex boolean, params text[]);
+create type route as (name text, method text, path express.path, handler text, priority integer);
+create type express.request as (
+    method omni_http.http_method,
+    path text,
+    query_string text,
+    body bytea,
+    headers omni_http.http_header[],
+    parameters jsonb,
+    query text[]
+);
+
+
+create or replace function http_request(
+    route jsonb,
+    request omni_httpd.http_request
+)
+returns express.request
+language plpgsql
+as $$
+declare
+    path_params jsonb := jsonb_build_object();
+    query_params text[] := '{}';
+    matches text[];
+    params text[] := array(select jsonb_array_elements_text(route->'path'->'params'));
+    i integer;
+begin
+    if (route->'path'->>'is_regex')::boolean and array_length(params, 1) > 0 then
+        matches := array(select (regexp_matches(request.path, route->'path'->>'value', 'g')));
+        for i in 1..array_length(params, 1)
+        loop
+            path_params := path_params || jsonb_build_object(params[i], matches[1][i]);
+        end loop;
+    end if;
+    if request.query_string is not null then
+        query_params := omni_web.parse_query_string(request.query_string);
+    end if;
+
+    return row(
+        request.method,
+        request.path,
+        request.query_string,
+        request.body,
+        request.headers,
+        path_params,
+        query_params
+    )::express.request;
+end;
+$$;
+
+
+create or replace function parse_path(path text)
+returns express.path
+language plpgsql
+as $$
+declare
+    is_regex boolean := false;
+    path_regex text := path;
+    params text[] := '{}';
+begin
+    -- check if the path contains express-style parameters
+    if path like '%/:%' then
+        -- replace :param with a regex capture group
+        path_regex := regexp_replace(path, ':(\w+)', '([^/]+)', 'g');
+        params := array(select unnest from (select unnest(regexp_matches(path, ':(\w+)', 'g')) from generate_series(1, 1)) s);
+        is_regex := true;
+    -- check if the path contains regular expression special characters
+    elsif path ~ '[.*+?$^|()]' then
+        is_regex := true;
+    end if;
+    -- return the path as a path_spec
+    return row(path_regex, is_regex, params)::express.path;
+end;
+$$;
+
 
 create or replace procedure add_route(name text, handler text, method text, path text, priority integer)
 language sql
@@ -15,7 +90,7 @@ as $$
         jsonb_insert(
             coalesce(current_setting('express.http_routes', true),'[]')::jsonb,
             '{0}',
-            to_jsonb(row(name, method, path, handler, priority)::express.route)
+            to_jsonb(row(name, method, express.parse_path(path), handler, priority)::express.route)
         )::text,
         false
     )
@@ -46,7 +121,7 @@ as $$
                 select
                     name,
                     concat(
-                        'select ', handler, '(request.*) ',
+                        'select ', handler, '(express.http_request(''', row_to_json(t)::text,''', request.*)) ',
                         'from request ',
                         'where ',
                         case 
@@ -55,14 +130,15 @@ as $$
                         end,
                         'and ',
                         case 
-                            when path is not null then concat('request.path = ''', path, ''' ')
+                            when path is not null and not (path).is_regex then concat('request.path = ''', (path).value, ''' ')
+                            when path is not null and (path).is_regex then concat('request.path ~ ''', (path).value, ''' ')
                             else 'true '
                         end
                     ) as query,
                     priority
                 from
                     jsonb_to_recordset(coalesce(current_setting('express.http_routes', true),'[]')::jsonb)
-                    as t(method text, path text, handler text, priority integer, name text)
+                    as t(method text, path express.path, handler text, priority integer, name text)
             ) as routes(name, query, priority)
         )
 $$;
--- db/schema.sql
+++ db/schema.sql
@@ -1,12 +1,23 @@
 set search_path = public;
 
-create or replace function headers(request express.request) returns express.outcome
+create or replace function full_info(request express.request) returns express.outcome
 language plpgsql
 as $$
 begin
-    return omni_httpd.http_response(body => request.headers::text);
+    return omni_httpd.http_response(
+        body => jsonb_build_object(
+            'method', request.method::text,
+            'path', request.path,
+            'query_string', request.query_string,
+            'body', convert_from(request.body, 'UTF8'),
+            'headers', array_to_json(request.headers),
+            'parameters', request.parameters,
+            'query', request.query
+        )::text
+    );
 end;
 $$;
+
 
 create or replace function default_handler(request express.request) returns express.outcome
 language plpgsql
@@ -18,5 +29,11 @@ $$;
 
 call express.reset_http_routes();
 call express.add_route('not_found', 'default_handler', null, null, 0 );
-call express.get('/headers', 'headers', 1);
+call express.get('/info', 'full_info', 1); -- exact match
+call express.get('^/(route)?information$', 'full_info', 1); -- regex match
+call express.get('/article/:id/:page', 'full_info', 1); -- regex match with params
 call express.load_http_routes();
+-- select current_setting('express.http_routes', true);
+-- select * from omni_httpd.handlers;
+
+
